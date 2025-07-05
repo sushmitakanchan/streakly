21eaaf8678df68da0189e6d8cd9987e6346f8a64
Sushmita <sushmitakanchan622@gmail.com>
2025-07-05 15:21:09 +0530

    feat: problem table and update problem controller

--- backend/controllers/problem.controller.js
+++ backend/controllers/problem.controller.js
@@ -67,7 +67,17 @@ export const createProblem = async(req, res)=>{
 
 export const getAllProblems = async(req, res)=>{
     try {
-        const problem = await db.problem.findMany();
+        const problem = await db.problem.findMany(
+            {
+                include:{
+                    solvedBy:{
+                        where:{
+                            userId: req.user.id
+                        }
+                    }
+                }
+            }
+        );
 
         if(!problem){
             return res.status(404).json({
